7c92638bbb2372fdc99257713585562b4ac62f48
Thang Nguyen <quocthangn1990@gmail.com>
2022-07-19 15:41:19 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.use('/login', (req, res) => {
+    res.send({
+        token: 'test123'
+    });
+});
+
+app.listen(8080, () => console.log('API is running on http://localhost:8080/login'));
